a45b9bcec07890d10ee2aa13162099815b9815d5
Hyungryul Kim <rancet@gmail.com>
2013-08-08 13:27:16 +0900

    Create rock_paper_scissors_game.js
    가위바위보 게임인데, 에러 나네요.
    
    error message: Your code doesn't look quite right. You got the following error: ReferenceError: Choice1 is not defined

---
+++ src/rock_paper_scissors_game.js
@@ -0,0 +1,47 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+  computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}
+        
+var compare = function(choice1, choice2){
+    var userChoice = choice1;
+    var computerChoice = choice2;
+    
+    if (Choice1 === Choice2)     {
+        return "The result is a tie!";
+    }
+    
+    if (choice1 === "rock") {
+        if (choice2 === "scissors"){
+            return "rock wins";
+        }
+        else {
+            return "paper wins";    
+        }
+    }
+    
+    if (choice1 === "paper"){
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors win";
+        }
+    }
+    
+    if (choice1 === "scissors"){
+        if (choice2 === "rock") {
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+};
+
+compare("rock", computerChoice);
